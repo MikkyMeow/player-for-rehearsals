8404b36e1e29016f967bf79e8f31cea88bd66344
Mikky <arturakk2015@gmail.com>
2020-07-09 19:24:39 +0500

    add audio controls script

--- js/app.js
+++ js/app.js
@@ -1,61 +1,45 @@
-let addPoint = document.querySelector('.add-point');
-let minusSecond = document.querySelector('.minus-second');
-let pointList = document.querySelector('.point-list');
-let playBtn = document.querySelector('.play');
-let pauseBtn = document.querySelector('.pause');
-let stopBtn = document.querySelector('.stop');
-let selectTrack = document.querySelector('.select-track');
-let tracks = document.querySelectorAll('.track-item');
-let trackList = document.querySelector('.track-list');
-let currentTrack = document.querySelector('.current');
+let audio = new Audio()
+audio.src = 'audio/SMG - No Sleep Till Hell.mp3'
 
-
-let audio = new Audio();
-audio.src="audio/SMG - No Way Back.mp3"
-
-playBtn.addEventListener('click', function() {
-  audio.play();
-});
-
-pauseBtn.addEventListener('click', function() {
-  audio.pause();
-});
-
-stopBtn.addEventListener('click', function() {
-  audio.pause();
-  audio.currentTime = 0;
-});
-
-addPoint.addEventListener('click', function () {
-  let li = document.createElement('li');
-  li.setAttribute('class', 'point-item');
-  li.innerHTML = audio.currentTime.toFixed(2);
-  pointList.append(li);
-});
-
-minusSecond.addEventListener('click', function() {
-  audio.currentTime -= 1;
+//audio controls
+document.querySelector('.play').addEventListener('click', function() {
+  audio.play()
+})
+document.querySelector('.pause').addEventListener('click', function() {
+  audio.pause()
+})
+document.querySelector('.stop').addEventListener('click', function() {
+  audio.pause()
+  audio.currentTime = 0
 })
 
-pointList.addEventListener('mouseover', function() {
-  let pointItem = document.querySelectorAll('.point-item');
-  for (let i = 0; i < pointItem.length; i++) {
-    pointItem[i].addEventListener('click', function() {
-      audio.currentTime = pointItem[i].innerHTML;
-    });
-  }
-});
+//rewind controls
+let rewindBtn = document.querySelectorAll('.rewind-control');
 
-selectTrack.addEventListener('click', function() {
-    trackList.classList.add('active');
-});
+for(let i = 0; i < rewindBtn.length; i++) {
+  rewindBtn[i].addEventListener('click', function() {
+    let btnVar = rewindBtn[i].innerHTML.substring(0, rewindBtn[i].innerHTML.length - 1)
+    audio.currentTime += parseInt(btnVar);
+  })
+}
 
-trackList.addEventListener('mouseover', function() {
-  for (let i = 0; i < tracks.length; i++) {
-    tracks[i].addEventListener('click', function() {
-      audio.src = 'audio/' + tracks[i].innerHTML + '.mp3';
-      trackList.classList.remove('active');
-      currentTrack.innerHTML = tracks[i].innerHTML;
-    });
-  }
-});
+//audio range
+audio.addEventListener('loadedmetadata', function() {
+  let currentSeconds = document.querySelector('.current-seconds')
+  let duration = document.querySelector('.duration')
+  let rangeControl = document.querySelector('.range-control')
+
+  audio.addEventListener('timeupdate', function() {
+    currentSeconds.innerHTML = convertTime(audio.currentTime)
+    rangeControl.style.width = audio.currentTime / audio.duration * 100 + '%';
+  })
+  duration.innerHTML = convertTime(audio.duration)
+  
+})
+
+//convert seconds to date
+let convertTime = function(sec) {
+  let date = new Date(null)
+  date.setSeconds(Math.round(sec))
+  return date.toISOString().substr(14, 5)
+}
