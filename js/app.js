06952a69cbf391a04bbed4b395d932d64fe94731
Mikky <arturakk2015@gmail.com>
2020-07-16 19:17:08 +0500

    add opacity and base animation for lists

--- js/app.js
+++ js/app.js
@@ -110,6 +110,11 @@ for(let i = 0; i < menuBtn.length; i++) {
     }
     menuBtn[i].classList.add('active')
     infoList[i].classList.add('active')
+    activeCount = i
+    
+    infoList[i].style.animationName = "right"
+    infoList[i].style.animationDuration = ".3s"
+    infoList[i].style.animationDirection = "reverse"
   })
 }
 
